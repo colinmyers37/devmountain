dcef46d8ae29e61687c8795cdc47e72b83951026
colinmyers37 <colinmyers37@gmail.com>
2022-07-18 09:32:29 -0500

    problem solved

--- week2/day1/challenge-add-to-zero/addToZero.js
+++ week2/day1/challenge-add-to-zero/addToZero.js
@@ -1,4 +1,13 @@
 // Starting array
 let array = [28, 43, -12, 30, 4, 0, 12]
 
-// Write your solution below:
+let value = false;
+for(let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+        if (array[i] + array[i] === 0) {
+            value = true;
+            break 
+        }
+    }
+}
+console.log(value)
