99d30b939998f787ad052fe777d993f29efb2418
colinmyers37 <colinmyers37@gmail.com>
2022-07-21 09:28:12 -0500

    morning challenge finished

---
+++ week2/day4/challenge-is-palindrome/isPalindrome.js
@@ -0,0 +1,19 @@
+//const isPalindrome = str => str === str.split('').reverse().join('')
+
+
+function isPal(str) {
+    let newStr = ('');
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+   if(newStr === str) {
+    console.log("true")
+   } else {
+    console.log("false")
+   }
+}
+isPal("")
+
+
+
+
