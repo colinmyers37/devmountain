231a4292dd38730bb98395f22d35463688039979
colinmyers37 <colinmyers37@gmail.com>
2022-07-28 08:59:48 -0500

    morning lab

---
+++ week3/day4/challenge-print-digits/printDigits.js
@@ -0,0 +1,12 @@
+function printDigits(num) {
+    let revNum = 0;
+    let lastDigit;
+while(num != 0){
+    lastDigit = num % 10;
+    revNum = revNum * 10 + lastDigit;
+    num = Math.floor(num/10);
+}
+return revNum
+}
+
+console.log(printDigits(456799))
