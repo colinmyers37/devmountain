bd7eb9da2e7a3585176f8fcf487152b3da6aef5a
colinmyers37 <colinmyers37@gmail.com>
2022-08-02 14:41:00 -0500

    controller and routes lab

---
+++ week4/day2/back-end-2/server/controller.js
@@ -0,0 +1,40 @@
+
+const { response } = require("express");
+const { request } = require("http");
+const HOUSES = require("./db.json");
+let base_id = 4;
+
+module.exports = {
+    getHouses: (req, res) => {
+        res.status(200).send(HOUSES);
+    },
+    deleteHouse: (req, res) => {
+        let index = HOUSES.findIndex( houses => houses.id === +req.params.id);
+        HOUSES.splice(index, 1);
+        res.status(200).send(HOUSES);
+    },
+    createHouse: (req, res) => {
+    let { address, price, imageURL } = req.body;
+    let new_house = {
+        id: base_id,
+        address,
+        price,
+        imageURL
+    }
+    HOUSES.push(new_house)
+    base_id++
+    console.log(HOUSES)
+    res.status(200).send(HOUSES)
+    },
+    updateHouse: (req, res) => {
+let { id } = req.params;
+let { type } = req.body;
+let index = HOUSES.findIndex( houses => houses.id === +id);
+if(HOUSES[index].price === 0 && type === "minus") {
+    console.log("Price is too low");
+    res.status(405).send(HOUSES);
+    return;
+}
+type === "plus" ? HOUSES[index].price += 10000 : HOUSES[index].price -= 10000
+    },
+}
