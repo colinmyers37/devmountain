a529f182b418c7ff4b6944fbadcd16733cb03719
colinmyers37 <colinmyers37@gmail.com>
2022-08-08 09:45:28 -0500

    morning challenge

---
+++ week5/day1/chal-outlier/outlier.js
@@ -0,0 +1,17 @@
+// function findOutlier (arr) {
+//     let evenArr = []
+//     let oddArr = []
+//     for (let i = 0; i<arr.length;i++){
+//         if(arr[i]%2===0){
+//            evenArr.push( arr[i])
+//         }else{
+//             oddArr.push(arr[i])
+//         }
+//     }if(oddArr.length>evenArr.length){
+//         return evenArr
+//     }else{
+//         return oddArr
+//     }
+// }
+let test = [2,4,6,7]
+console.log(challengeFunction(test))
