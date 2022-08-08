ba0df39f39f182c2814897984c6d35048d945a3b
colinmyers37 <colinmyers37@gmail.com>
2022-08-08 15:59:23 -0500

    second lab

--- week5/day1/sql-basics/employee.sql
+++ week5/day1/sql-basics/employee.sql
@@ -1,8 +1,11 @@
-SELECT * FROM employee WHERE city = 'Calgary';
+SELECT first_name, last_name FROM employee WHERE city = 'Calgary';
 
 SELECT birth_date FROM employee 
 ORDER BY birth_date DESC
 LIMIT 1;
+
+-- SELECT MAX (birth_date)
+-- FROM employee
 
 SELECT birth_date FROM employee 
 ORDER BY birth_date
