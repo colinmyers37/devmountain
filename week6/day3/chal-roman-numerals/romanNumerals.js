19ca20ee1a5d3fe633870ec552ace433808bc2d7
colinmyers37 <colinmyers37@gmail.com>
2022-08-17 09:06:36 -0500

    finished monring challenge

---
+++ week6/day3/chal-roman-numerals/romanNumerals.js
@@ -0,0 +1,22 @@
+function toRoman(num) {
+    let roman = {
+      M: 1000,
+      D: 500,
+      C: 100,
+      L: 50,
+      X: 10,
+      V: 5,
+      I: 1
+    };
+    let str = '';
+  
+    for (let i of Object.keys(roman)) {
+      let q = Math.floor(num / roman[i]);
+      num -= q * roman[i];
+      str += i.repeat(q);
+    }
+  
+    return str;
+  }
+
+console.log(toRoman(267)) 
