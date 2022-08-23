ca7b632e23c6808fb43f182c2a71d7565568663c
colinmyers37 <colinmyers37@gmail.com>
2022-08-23 11:45:51 -0500

    finished morning

--- week7/day2/chal-road-trip/tripTime.js
+++ week7/day2/chal-road-trip/tripTime.js
@@ -1,105 +1,117 @@
-const tripTime = (arr) => {
-    let result = {
-        avgSpeedLimits: [], 
-        segmentTimes: [], 
-        totalTime: null
-    }
+// const tripTime = (arr) => {
+//     let result = {
+//         avgSpeedLimits: [], 
+//         segmentTimes: [], 
+//         totalTime: null
+//     }
 
 
-    return result
-}
+//     return result
+// }
 
-// UNCOMMENT THE FUNCTION CALL AT
-// THE BOTTOM BEFORE RUNNING THE FILE
-
-
-const stops = [
-    {
-        name: `Gus's Gas`, 
-        speedLimits: [
-            {
-                distance: 5,
-                speedLimit: 45
-            },
-            {
-                distance: 97,
-                speedLimit: 65
-            },
-            {
-                distance: 72,
-                speedLimit: 70
-            },
-            {
-                distance: 25,
-                speedLimit: 50
-            }
-        ], 
-        traffic: 12
-    }, 
-    {
-        name: `Halle's House of Pancakes`, 
-        speedLimits: [
-            {
-                distance: 36,
-                speedLimit: 50
-            },
-            {
-                distance: 141,
-                speedLimit: 75
-            }
-        ], 
-        traffic: 0
-    }, 
-    {
-        name: `Jake's Great Shakes`, 
-        speedLimits: [
-            {
-                distance: 100,
-                speedLimit: 75
-            },
-            {
-                distance: 84,
-                speedLimit: 70
-            },
-            {
-                distance: 20,
-                speedLimit: 75
-            }
-        ], 
-        traffic: 30
-    }, 
-    {
-        name: `Luna's Lunch Counter`, 
-        speedLimits: [
-            {
-                distance: 3,
-                speedLimit: 35
-            },
-            {
-                distance: 5,
-                speedLimit: 45
-            },
-            {
-                distance: 20,
-                speedLimit: 65
-            },
-            {
-                distance: 85,
-                speedLimit: 75
-            },
-            {
-                distance: 3,
-                speedLimit: 65
-            },
-            {
-                distance: 5,
-                speedLimit: 55
-            }
-        ], 
-        traffic: 7
-    }, 
-
-]
+// // UNCOMMENT THE FUNCTION CALL AT
+// // THE BOTTOM BEFORE RUNNING THE FILE
 
 
-tripTime(stops)
+// const stops = [
+//     {
+//         name: `Gus's Gas`, 
+//         speedLimits: [
+//             {
+//                 distance: 5,
+//                 speedLimit: 45
+//             },
+//             {
+//                 distance: 97,
+//                 speedLimit: 65
+//             },
+//             {
+//                 distance: 72,
+//                 speedLimit: 70
+//             },
+//             {
+//                 distance: 25,
+//                 speedLimit: 50
+//             }
+//         ], 
+//         traffic: 12
+//     }, 
+//     {
+//         name: `Halle's House of Pancakes`, 
+//         speedLimits: [
+//             {
+//                 distance: 36,
+//                 speedLimit: 50
+//             },
+//             {
+//                 distance: 141,
+//                 speedLimit: 75
+//             }
+//         ], 
+//         traffic: 0
+//     }, 
+//     {
+//         name: `Jake's Great Shakes`, 
+//         speedLimits: [
+//             {
+//                 distance: 100,
+//                 speedLimit: 75
+//             },
+//             {
+//                 distance: 84,
+//                 speedLimit: 70
+//             },
+//             {
+//                 distance: 20,
+//                 speedLimit: 75
+//             }
+//         ], 
+//         traffic: 30
+//     }, 
+//     {
+//         name: `Luna's Lunch Counter`, 
+//         speedLimits: [
+//             {
+//                 distance: 3,
+//                 speedLimit: 35
+//             },
+//             {
+//                 distance: 5,
+//                 speedLimit: 45
+//             },
+//             {
+//                 distance: 20,
+//                 speedLimit: 65
+//             },
+//             {
+//                 distance: 85,
+//                 speedLimit: 75
+//             },
+//             {
+//                 distance: 3,
+//                 speedLimit: 65
+//             },
+//             {
+//                 distance: 5,
+//                 speedLimit: 55
+//             }
+//         ], 
+//         traffic: 7
+//     }, 
+
+// ]
+
+
+// tripTime(stops)
+
+
+
+// function fibo(num) {
+//     if (num < 2) {
+//       return num
+//     } else {
+//       return fibo(num - 1) + fibo(num - 2)
+//     }
+//   }
+
+//   console.log(fibo(40));
